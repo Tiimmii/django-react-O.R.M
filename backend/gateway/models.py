66ddf6cf2590c67287972c0a0810c5e99adc9487
Daniel Oluwole <danieloluwoleoluwatimileyin@gmail.com>
2023-06-29 12:21:38 +0100

    Backend: gateway

---
+++ backend/gateway/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from customuser.models import Customuser
+
+# Create your models here.
+class Jwt(models.Model):
+    user = models.OneToOneField(Customuser, related_name='logged_in_user', on_delete=models.CASCADE)
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
